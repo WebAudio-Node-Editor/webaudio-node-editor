fc598cef96a0d87d77ff6f935885d659fd9899fd
Amelia Wissink <afw2122@columbia.edu>
2024-02-22 11:19:43 -0500

    Add pink noise as a new noise type option

--- src/rete/nodes/EditorNoiseNode.tsx
+++ src/rete/nodes/EditorNoiseNode.tsx
@@ -14,6 +14,7 @@ export class EditorNoiseNode extends Classic.Node<
         const dropdownOptions = [
             { value: 'White Noise', label: 'White Noise' },
             { value: 'Brown Noise', label: 'Brown Noise' },
+            { value: 'Pink Noise', label: 'Pink Noise' },
         ]
         this.addControl(
             'noiseType',
@@ -48,6 +49,21 @@ export class EditorNoiseNode extends Classic.Node<
                 output[i] = (lastOut + 0.02 * brown) / 1.02
                 lastOut = output[i]
                 output[i] *= 3.5
+            }
+        } else if (this.controls.noiseType.value == 'Pink Noise') {
+            let b0, b1, b2, b3, b4, b5, b6;
+            b0 = b1 = b2 = b3 = b4 = b5 = b6 = 0.0;
+            for (let i = 0; i < bufferSize; i++) {
+                let white = Math.random() * 2 - 1;
+                b0 = 0.99886 * b0 + white * 0.0555179;
+                b1 = 0.99332 * b1 + white * 0.0750759;
+                b2 = 0.96900 * b2 + white * 0.1538520;
+                b3 = 0.86650 * b3 + white * 0.3104856;
+                b4 = 0.55000 * b4 + white * 0.5329522;
+                b5 = -0.7616 * b5 - white * 0.0168980;
+                output[i] = b0 + b1 + b2 + b3 + b4 + b5 + b6 + white * 0.5362;
+                output[i] *= 0.11;
+                b6 = white * 0.115926;
             }
         }
         noiseSource.buffer = noiseBuffer
