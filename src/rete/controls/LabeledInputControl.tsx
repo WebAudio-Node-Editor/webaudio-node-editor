af75a058348f664ea78db86d1e02346b075c86eb
Ryan Xu <ryan-xu@live.com>
2024-03-20 19:45:00 -0400

    Formatting and blog changes

--- src/rete/controls/LabeledInputControl.tsx
+++ src/rete/controls/LabeledInputControl.tsx
@@ -3,17 +3,15 @@ import { Drag } from 'rete-react-plugin'
 import * as React from 'react'
 import styled from 'styled-components'
 
-
-
 const Input = styled.input<{ styles?: (props: any) => any }>`
-  width: 100%;
-  border-radius: 30px;
-  background-color: white;
-  padding: 2px 6px;
-  border: 1px solid #999;
-  font-size: 110%;
-  box-sizing: border-box;
-  ${props => props.styles && props.styles(props)}
+    width: 100%;
+    border-radius: 30px;
+    background-color: white;
+    padding: 2px 6px;
+    border: 1px solid #999;
+    font-size: 110%;
+    box-sizing: border-box;
+    ${(props) => props.styles && props.styles(props)}
 `
 
 export class LabeledInputControl extends Classic.InputControl<'number'> {
@@ -27,35 +25,39 @@ export class LabeledInputControl extends Classic.InputControl<'number'> {
         super('number', { initial: value, readonly: Boolean(readonly), change })
     }
 }
-export function CustomLabeledInputControl(props: { data: LabeledInputControl, styles?: () => any }) {
-  const [value, setValue] = React.useState(props.data.value)
-  const ref = React.useRef(null)
-  Drag.useNoDrag(ref)
+export function CustomLabeledInputControl(props: {
+    data: LabeledInputControl
+    styles?: () => any
+}) {
+    const [value, setValue] = React.useState(props.data.value)
+    const ref = React.useRef(null)
+    Drag.useNoDrag(ref)
 
-  React.useEffect(() => {
-    setValue(props.data.value)
-  }, [props.data.value])
+    React.useEffect(() => {
+        setValue(props.data.value)
+    }, [props.data.value])
 
-  return (
-    <Input
-      value={value}
-      type={props.data.type}
-      ref={ref}
-      readOnly={props.data.readonly}
-      onChange={e => {
-        const val = (props.data.type === 'number'
-          ? +e.target.value
-          : e.target.value) as typeof props.data['value']
+    return (
+        <Input
+            value={value}
+            type={props.data.type}
+            ref={ref}
+            readOnly={props.data.readonly}
+            onChange={(e) => {
+                const val = (
+                    props.data.type === 'number'
+                        ? +e.target.value
+                        : e.target.value
+                ) as (typeof props.data)['value']
 
-        setValue(val)
-        props.data.setValue(val)
-      }}
-      step = {props.data.increment}
-      styles={props.styles}
-    />
-  )
+                setValue(val)
+                props.data.setValue(val)
+            }}
+            step={props.data.increment}
+            styles={props.styles}
+        />
+    )
 }
-
 
 /*
 export class LabeledInputControl extends Classic.InputControl<'number'> {
