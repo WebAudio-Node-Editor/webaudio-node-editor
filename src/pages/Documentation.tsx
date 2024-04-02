46ad10861cdb5a835ca5b964e8c1b31d5cb4cfd4
Arthi Krishna <krishnart07@gmail.com>
2024-04-02 17:30:44 -0400

    Re-structured Documentation for Basic Usage

--- src/pages/Documentation.tsx
+++ src/pages/Documentation.tsx
@@ -1,8 +1,110 @@
 import { Layout, Flex, Divider } from 'antd'
 import { Link } from 'react-router-dom'
 import './Blog.css'
+import React from 'react';
 
 export default function Documentation() {
+    class DocumentContent extends React.Component{
+        state = {
+            active: "basicUsage"
+        }
+
+        updateState(newState: string) {
+            this.setState({ active: newState })
+        }
+
+        render(){
+            return (
+                <div>
+                    <Layout
+                    style={{
+                        margin: '10px 10px',
+                        backgroundColor: 'rgba(0,0,0,0.2)',
+                    }}
+                    >
+                        <div style={{textAlign: "center"}}>
+                            <div className={(this.state.active === "basicUsage") ? "Blog-nav-active" : "Blog-nav"} onClick={() => this.updateState("basicUsage")}>Basic Usage</div>
+                            <div className="Blog-nav" onClick={() => this.updateState("nodes")}>Node Types</div>
+                            <div className="Blog-nav" onClick={() => this.updateState("keyboard")}>Keyboard</div>
+                        </div>
+                    </Layout> 
+                    
+                    <div style={{ display: (this.state.active === "basicUsage") ? "block" : "none" }}>
+                        <Layout className="Block container">
+                            <div className="Block-header">What is this?</div>
+                            <div className="Block-content">This is a node-based editor for WebAudio created using Rete.js, a Javascript framework for visual programming through node editors. Node editors are widely used in shader editors and even game programming (as in Unreal Engine's Blueprints).</div>
+                        </Layout>
+
+                        <Layout className="Block container">
+                            <div className="Block-header" style={{ marginBottom: "20px"}}>Basic Usage</div>
+                            <div className="row" style={{ margin: "0px"}}>
+                                <div className="Half-block">
+                                    <div className="Block-content"><b>Create Connections: </b> Click + drag from socket to socket.</div>
+                                    <img src={require('../images/Connecting Nodes.gif')} alt="Adding Nodes" width="100%"/>
+                                </div>
+                                <div className="Half-block">
+                                    <div className="Block-content"><b>Create New Nodes: </b> Right click on empty space to open the context menu</div>
+                                    <img src={require('../images/Adding Nodes.gif')} alt="Adding Nodes" width="100%"/>
+                                </div>
+                            </div>
+                            <div className="row" style={{ margin: "0px"}}>
+                                <div className="Half-block">
+                                    <div className="Block-content"><b>Delete Nodes: </b> Right-click on a node</div>
+                                    <img src={require('../images/Deleting Nodes.gif')} alt="Adding Nodes" width="100%"/>
+                                </div>
+                                <div className="Half-block">
+                                    <div className="Block-content"><b>Organize Nodes: </b> Click the "Auto-Arrange" button in the upper right corner</div>
+                                    <img src={require('../images/Arrange Nodes.gif')} alt="Adding Nodes" width="100%"/>
+                                </div>
+                            </div>                            
+                        </Layout>
+                        <div className="row">
+                            <Layout className="container Half-block">
+                                <div className="Block-header">Input Behavior</div>
+                                <div className="Block-content">
+                                    When an input (with a socket) also has an editable field, such as the
+                                    Base Frequency field of an oscillator node, adding a connection to the socket
+                                    will <b>overwrite the value</b> in the editable field.
+                                </div>
+                                <div className="Block-content">
+                                When multiple signals are connected to a single socket, <b>they add additively</b>.
+                                That means if you pass two signals with values 300 and 500 into the Additional
+                                Frequency field of an oscillator node, the additional frequency will be 300+500=800.
+                                </div>
+                            </Layout>
+
+                            <Layout className="container Half-block">
+                                <div className="Block-header">Frequency vs. Audio Signals</div>
+                                <div className="Block-content" >Some inputs produce signals that are intended to be interpreted as
+                                    frequencies; these signals should therefore find their way to the
+                                    frequency field of an oscillator at some point.
+                                </div>
+                                <div className="Block-content">
+                                    Of course, it is valid to not use them this way, but the results might
+                                    not be useful!
+                                </div>
+                                <div className="Block-content">
+                                    Audio signals can also be interpreted as frequencies. However, because
+                                    audio signals are typically in the [-1, 1] range, you will often have to
+                                    pass them through a gain node to use them in this way. See the AM+FM
+                                    synthesis example for an example of how this is done.
+                                </div>
+                            </Layout>
+                            </div>
+                        </div>
+                        
+                        <div style={{ display: (this.state.active === "nodes") ? "block" : "none" }}>
+                            Node Types
+                        </div>
+                        
+                        <div style={{ display: (this.state.active === "keyboard") ? "block" : "none" }}>
+                            Keyboard
+                        </div>
+                </div> 
+            )
+        }
+    }
+
     return (
         <div
             style={{
@@ -13,7 +115,6 @@ export default function Documentation() {
                 zIndex: '-1',
             }}
         >
-            {/* <div style={{width: '100vw', height: '100vh', backgroundImage: "linear-gradient(to bottom right, CornflowerBlue, Pink)", position: "sticky", top: "0px", zIndex: "-1"}}/> */}
             <Layout
                 style={{
                     display: 'flex',
@@ -77,30 +178,10 @@ export default function Documentation() {
                     </a>
                     <div style={{ flexGrow: 1 }} />
                 </Flex>
-                <Layout
-                    style={{
-                        display: 'flex',
-                        width: '75%',
-                        overflow: 'auto',
-                        margin: 'auto',
-                        backgroundColor: 'rgba(0,0,0,0.2)',
-                        padding: '0em 2em',
-                    }}
-                >
-                    <div className="Blog-header">Basic Usage</div>
-                    <div className="Blog-content">
-                        Drag from socket to socket to create connections between
-                        nodes.
-                        <br />
-                        Right click on empty space to open the context menu,
-                        letting you create new nodes.
-                        <br />
-                        Right click on a node to open the context menu, letting
-                        you delete the node.
-                        <br />
-                        Use the auto-arrange button in the upper right to stay
-                        organized.
-                    </div>
+
+                <DocumentContent />
+
+                {/*
                     <div className="Blog-header">Basic Nodes</div>
                     <div className="Blog-content">
                         The most essential nodes fall into three categories:
@@ -122,38 +203,6 @@ export default function Documentation() {
                         node combines both visualizers, and audio output, and a
                         gain node. In addition to their normal function, these
                         nodes are also useful for debugging your signal.
-                    </div>
-                    <div className="Blog-header">Input behavior</div>
-                    <div className="Blog-content">
-                        When an input (with a socket) also has an editable
-                        field, such as the Base Frequency field of an oscillator
-                        node, adding a connection to the socket will{' '}
-                        <i>overwrite</i> the value in the editable field.
-                    </div>
-                    <div className="Blog-content">
-                        When multiple signals are connected to a single socket,
-                        they add <i>additively</i>. That means if you pass two
-                        signals with values 300 and 500 into the Additional
-                        Frequency field of an oscillator node, the additional
-                        frequency will be 300+500=800.
-                    </div>
-                    <div className="Blog-header">
-                        Frequency Signals vs Audio Signals
-                    </div>
-                    <div className="Blog-content">
-                        Some inputs produce signals that are intended to be
-                        interpreted as frequencies; these signals should
-                        therefore find their way to the frequency field of an
-                        oscillator at some point. Of course, it is valid to not
-                        use them this way, but the results might not be useful!
-                    </div>
-                    <div className="Blog-content">
-                        Note that audio signals can also be interpreted as
-                        frequencies. However, because audio signals are
-                        typically in the [-1, 1] range, you will often have to
-                        pass them through a gain node to use them in this way.
-                        See the AM+FM synthesis example for an example of how
-                        this is done.
                     </div>
                     <div className="Blog-header">Advanced Nodes</div>
                     <div className="Blog-subheader">Note Frequency</div>
@@ -239,7 +288,7 @@ export default function Documentation() {
                         and nonfiltered signal, through the use of a Keyboard
                         ADSR node.
                     </div>
-                </Layout>
+                </Layout> */}
             </Layout>
         </div>
     )
